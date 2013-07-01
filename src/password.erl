85ef5c9fdb4475c72d0716701fcc4bfab33fe454
ed <dontemailme@dontemailme.com>
2013-07-01 09:40:35 -0700

    spec

--- src/password.erl
+++ src/password.erl
@@ -4,16 +4,19 @@
 		 decode_password/1, 
 		 is_secure/1]).
 
+-spec encode_password(term())-> binary().
 encode_password(Value) ->
 	A = base64:encode(Value),
 	A1 = base64:encode(A),
 	base64:encode(A1).
 
+-spec decode_password(binary())-> term().
 decode_password(Value)->
 	B1 = base64:decode(Value),
 	B2 = base64:decode(B1),
 	base64:decode(B2).
 
+-spec is_secure(term())-> true | false.
 is_secure(Value) ->
 	R = encode_password(Value),
 	Value =:= decode_password(R).
