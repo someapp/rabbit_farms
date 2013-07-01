fd27bef3cb0ca12f50f2aa92827db7e910a8c6d7
ed <dontemailme@dontemailme.com>
2013-07-01 15:38:38 -0700

    er45t

--- src/rabbit_farm_keeper.erl
+++ src/rabbit_farm_keeper.erl
@@ -126,15 +126,17 @@ create_rabbit_farm_instance(#rabbit_farm{amqp_params    = AmqpParams,
 										  	begin 
 												{ok, Channel}           = amqp_connection:open_channel(Connection),
 												{'exchange.declare_ok'} = amqp_channel:call(Channel, Declare),
-												lager:log(info,"qdeclare ~p",[QDeclare]),
-												
+												lager:log(info,"exchange declared ok",[]),
 												R = amqp_channel:call(Channel, QDeclare),
 												{'queue.declare_ok', _, _, _} = R,
-												lager:log(info,"qdeclare ~p with return ~p",[QDeclare, R]),
-												lager:log(info,"qdeclare ~p",[BindQueue]),
+
+												lager:log(info,"queue declared ok ~p",[R]),
+											
 												R1 = amqp_channel:call(Channel, BindQueue),
-												lager:log(info,"Bindqueue ~p with return ~p",[BindQueue, R1]),
 												{'queue.bind_ok'} =R1,
+
+												lager:log(info,"queue bind ok ~p",[R1]),
+												
 												Channel
 										    end
 									  	    || _I <-lists:seq(1,ChannelCount)]
