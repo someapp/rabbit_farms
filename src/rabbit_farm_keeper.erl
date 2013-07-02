47416f6701937fd91f4f44a94c8c772ea1b78b43
ed <dontemailme@dontemailme.com>
2013-07-02 10:33:07 -0700

    clsoe connectinon on process alive

--- src/rabbit_farm_keeper.erl
+++ src/rabbit_farm_keeper.erl
@@ -99,8 +99,8 @@ terminate(_Reason, State) ->
 		 false->
 		 		FarmName = State#rabbit_farm.farm_name,
 				lager:log(error,"the farm ~p: ~p~n",[FarmName, {error, farm_died}])
-	end,
-	amqp_connection:close(Connection),
+	end
+	
 	ok.
 
 code_change(_OldVsn, State, _Extra) ->
