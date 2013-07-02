d9b08445fe30da37055c6d46f70062cc7fc189f2
ed <dontemailme@dontemailme.com>
2013-07-02 16:30:03 -0700

    c

--- src/password.erl
+++ src/password.erl
@@ -1,9 +1,9 @@
 -module(password).
 
 -export([encode_password/1,
-		 encode_password/2,
+%		 encode_password/2,
 		 decode_password/1, 
-		 decode_password/2,
+%		 decode_password/2,
 		 is_secure/1]).
 
 -spec encode_password(term())-> binary().
@@ -12,19 +12,9 @@ encode_password(Value) ->
 	A1 = base64:encode(A),
 	base64:encode(A1).
 
--spec encode_password(term(), atom())-> binary().
-encode_password(Value,Encrypt) ->
- 	Encrypt:
- 	.
 
 -spec decode_password(binary())-> term().
 decode_password(Value)->
-	B1 = base64:decode(Value),
-	B2 = base64:decode(B1),
-	base64:decode(B2).
-
--spec decode_password(binary(),atom())-> term().
-decode_password(Value, Decrypt)->
 	B1 = base64:decode(Value),
 	B2 = base64:decode(B1),
 	base64:decode(B2).
