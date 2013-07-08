a80594b387104a9ff328598c1afc1007cb85de74
ed <dontemailme@dontemailme.com>
2013-07-07 18:57:11 -0700

    1

--- include/spark_restc_config.hrl
+++ include/spark_restc_config.hrl
@@ -1,18 +1,18 @@
--record(spark_restc_config{
+-record(spark_restc_config, {
 	spark_api_endpoint = [],
 	spark_app_id = [],
 	spark_brand_id = [],
 	spark_client_secret =[],
 	spark_create_oauth_accesstoken =
-              "/brandId/{brandId}/oauth2/accesstoken/application/{applicationId}"},
+              "/brandId/{brandId}/oauth2/accesstoken/application/{applicationId}",
 	auth_profile_miniProfile =
-              "/brandId/{brandId}/profile/attributeset/miniProfile/{targetMemberId}"},
+              "/brandId/{brandId}/profile/attributeset/miniProfile/{targetMemberId}",
 	profile_memberstatus =
-              "/brandId/{brandId}/application/{applicationId}/member/{memberId}/status"},
+              "/brandId/{brandId}/application/{applicationId}/member/{memberId}/status",
 	community2brandId =
               [{spark,"1","1001"},
-     	jdate,"3","1003"},
-     	cupid,"10","1015"},
-     	bbw,"23","90410"},
-     	blacksingle,"24","90510"}]}
+     	{jdate,"3","1003"},
+     	{cupid,"10","1015"},
+     	{bbw,"23","90410"},
+     	{blacksingle,"24","90510"}]}
 }).
